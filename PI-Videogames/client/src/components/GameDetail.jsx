5e96a49b8f1a66be2b7c7102262ae98af4fad192
Superjonico <leandrobuzeta@gmail.com>
2021-08-09 12:53:08 -0300

    details

--- PI-Videogames/client/src/components/GameDetail.jsx
+++ PI-Videogames/client/src/components/GameDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './gamedetail.css';
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 
@@ -10,8 +10,7 @@ export default function GameDetail (){
     //dispatchar una accion llamando a la ruta del back con el id o llamar al back con el id del juego
     const { id } = useParams()
    
-  
-
+    console.log(game)
     useEffect(() => {
         async function getVideogameById(id){
             const game = await axios.get(`http://localhost:3001/videogames/${id}`)
@@ -21,13 +20,25 @@ export default function GameDetail (){
     })
     console.log(id)
     return (
-        <div className ="back">
-            
-                {
-                  <h1>{game.name}</h1>
-
-                }
-            
-        </div>
+            <div>
+                <Link to= "/home">
+                <button className ="btnhome">Home</button>
+                </Link>
+              {
+                  game ?
+                  <div>
+                    <div className ="tit"> 
+                        <h1 >{game.name}</h1>
+                    </div> 
+                        <div className ="contimg">
+                            <img src={game.background_image} alt="imagen" className ="image" /> 
+                        </div>
+                        <div className="continfo">
+                            <p>{game.description}</p>
+                        </div> 
+                  </div>
+                  : <h2> There is no game with that id. Sorry!</h2>
+              }
+            </div> 
     )
 }
