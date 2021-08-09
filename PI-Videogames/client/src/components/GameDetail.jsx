db314c096439c57689cf0615e252e6da72c3d99b
Superjonico <leandrobuzeta@gmail.com>
2021-08-09 13:28:37 -0300

    details good

--- PI-Videogames/client/src/components/GameDetail.jsx
+++ PI-Videogames/client/src/components/GameDetail.jsx
@@ -17,18 +17,29 @@ export default function GameDetail (){
               setGame(game.data)
            }
         getVideogameById(id)
-    })
+    },[])
     console.log(id)
     return (
             <div>
-                <Link to= "/home">
-                <button className ="btnhome">Home</button>
-                </Link>
+                
               {
                   game ?
                   <div>
                     <div className ="tit"> 
                         <h1 >{game.name}</h1>
+
+                        <div className ="contback">
+                            <Link to= "/home">
+                            <button className ="btnhome">Home</button>
+                            </Link>
+                        </div>
+                        
+                    </div>
+                    <div className ="contextra">
+                        <h2>{game.rating}</h2   >
+                        {game.genres.map((genre) => {
+                            return <p>{genre.name}</p>
+                        })}
                     </div> 
                         <div className ="contimg">
                             <img src={game.background_image} alt="imagen" className ="image" /> 
